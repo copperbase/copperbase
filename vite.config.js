ac0ba762af54dda64fea267308dfd5f70ed59843
ably <ademmurat1616@gmail.com>
2025-12-16 21:15:05 +0300

    update: vite.config,jsconfig and more

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,64 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    cssCodeSplit: true,
+    minify: true,
+    minifyCSS: true,
+
+    rolldownOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: ({name}) => {
+          if (name && name.endsWith('.css')) {
+            return 'assets/css/[name]-[hash][extname]'
+          }
+          if (name && name.endsWith('.css')) {
+            return 'assets/css/[name]-[hash][extname]'
+          }
+          if (name && name.endsWith('.png') && name.endsWith('.jpg')) {
+            return 'assets/images/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') && id.includes('react-dom')) {
+              return 'react'
+            }
+          }
+          return 'vendor'
+        },
+      }
+    },
+
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            removeComments: true,
+            preset: 'default',
+          })
+        ]
+      }
+    }
+  }
+});
